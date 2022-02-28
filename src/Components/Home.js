6b84e0ab63ba19e4dd27a8baef99be0d448acf64
Ananya291 <ananya27042000@gmail.com>
2022-02-28 23:15:25 +0530

    passing props

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,34 @@ import Section from './Section'
 function Home() {
     return (
         <Container className='home'>
-            <Section/>
+            <Section
+                title="Model S"
+                description="Order Online for Cashless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                 title="Model Y"
+                 description="Order Online for Cashless Delivery"
+                 backgroundImg="model-y.jpg"
+                 leftBtnText="Custom Order"
+                 rightBtnText="Existing Inventory"
+            />
+            <Section
+                 title="Model 3"
+                 description="Order Online for Cashless Delivery"
+                 backgroundImg="model-3.jpg"
+                 leftBtnText="Custom Order"
+                 rightBtnText="Existing Inventory"
+            />
+            <Section
+                 title="Model X"
+                 description="Order Online for Cashless Delivery"
+                 backgroundImg="model-x.jpg"
+                 leftBtnText="Custom Order"
+                 rightBtnText="Existing Inventory"
+            />
         </Container>
     )
 }
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -2,13 +2,24 @@ import React from 'react'
 import './Section.css'
 import styled from 'styled-components'
 
-function Section() {
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
-        <Wrap>
+        <Wrap bgImage={backgroundImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for Cashless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        {leftBtnText}
+                    </LeftButton>
+                    <RightButton>
+                        {rightBtnText}
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -22,7 +33,49 @@ const Wrap = styled.div`
     background-position:center;
     background-repeat:no-repeat;
     background-image:url('/images/model-s.jpg');
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
+    background-image:${props => `url("/images/${props.bgImage}")`}
+
 `
 const ItemText = styled.div`
-// margin-left:40%;
+    padding-top:15vh;
+    text-align:center;
+`
+const ButtonGroup = styled.div`
+    display:flex;
+    margin-bottom:30px;
+    @media (max-width:768px){
+        flex-direction:column;
+    }
+
+`
+const LeftButton = styled.div`
+    background-color:rgba(23,26,32,0.8);
+    height:40px;
+    width:256px;
+    color:white;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    border-radius:100px;
+    opacity:0.85;
+    text-transform:uppercase;
+    font-size:12px;
+    cursor:pointer;
+    margin:8px;
+`
+const RightButton = styled(LeftButton)`
+    background:white;
+    opacity:0.65;
+    color:black;
+`
+const DownArrow = styled.img`
+    height:40px;
+    overflow-x: hidden;
+    animation:animateDown infinite 1.5s;
+`
+const Buttons = styled.div`
 `
