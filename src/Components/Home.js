4d33c28d274c38ae02e4526b49d61d2ea9c8488f
Ananya291 <ananya27042000@gmail.com>
2022-03-02 12:02:58 +0530

    ADD: Sections for the TESLA

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -34,6 +34,26 @@ function Home() {
                  leftBtnText="Custom Order"
                  rightBtnText="Existing Inventory"
             />
+            <Section
+                 title="Lowest Cost Solar Panels in America"
+                 description="Money-back guarantee"
+                 backgroundImg="solar-panel.jpg"
+                 leftBtnText="Order Now"
+                 rightBtnText="Learn More"
+            />
+            <Section
+                 title="Solar for New Roofs"
+                 description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+                 backgroundImg="solar-roof.jpg"
+                 leftBtnText="Order Now"
+                 rightBtnText="Learn More"
+            />
+            <Section
+                 title="Accessories"
+                 description=""
+                 backgroundImg="accessories.jpg"
+                 leftBtnText="Order Now"
+            />
         </Container>
     )
 }
